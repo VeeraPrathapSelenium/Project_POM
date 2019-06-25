548200d1e8fb5b4acbc2c390a2b81c95bad2be58
ranjith <knsrk45@gmail.com>
2019-06-25 07:08:29 +0530

    IsDisplayed method is added to generic class

---
+++ src/com/genericmethod/GenericMethods.java
@@ -0,0 +1,40 @@
+package com.genericmethod;
+
+import org.openqa.selenium.WebElement;
+
+public class GenericMethods {
+
+	/**
+	 @name:IsDisplayed
+	 @Purpose:This method is used for is Element successfully displayed on the application
+	 @Input:WebElement element,String elementname,String Pagename
+	 @Output:boolean
+	 @author:ranjith
+	 **/
+	
+	public boolean IsDisplayed(WebElement element,String elementname,String Pagename)
+	{
+		boolean status = true;
+		
+		try
+		{
+			boolean isDisplayeStatus = element.isDisplayed();
+			if(isDisplayeStatus)
+			{
+				System.out.println("The element"+elementname+ "is successfully displayed on page"+Pagename);
+			}
+			else
+			{
+				System.out.println("The element"+elementname+ "is not successfully displayed on page"+Pagename);
+				System.out.println("unable to find the element"+elementname);
+				status=false;
+			}
+		}
+		catch(Exception e)
+		{
+		System.out.println(e.getMessage());
+			
+		}
+		return status;
+	}
+}
